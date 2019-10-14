b64489459692004d4ae9d7f589daa4ca31b9ec29
martinspilak <martinecko24@gmail.com>
2019-10-14 08:32:00 +0200

    open-lab-01.3

---
+++ open-lab-01.3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace open_lab_01._3
+{
+    class Program
+    {
+        public static int sum(int a)
+        {
+            return ++a;
+        }
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(sum(a));
+        }
+
+
+
+
+
+    }
+}
